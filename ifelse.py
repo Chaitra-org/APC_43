227d323756361d07f9e41e509641253302cb8731
Chaitra-org <chaitralikasar@gmail.com>
2026-07-23 12:39:47 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,86 @@
+#  date: 22july 2026 wednesday 
+# 1. wap that reads the value of and check the number is 0 or non 0
+# 2. find largest number from 2 no. accept them from user
+# 3. take a input no. and check wheather +ve or -ve 
+# 4. take input char and check wheather its vowel or consonant 
+# 5. wap to evaluate percet grade>=90 = excelleent 
+# %>80
+# %>=70
+# %>=60 : Poor
+# 6. find largest AND SMALLEST amoung 3 no.
+# 7. even or not
+# 8. leap year
+
+# 1
+# x=int(input("enter a num"))
+# if x==0:
+#     print("number is 0")
+# else:
+#     print("non zero")
+
+# # 2
+# a=int(input("enter 1st num"))
+# b=int(input("enter 2nd num"))
+# if a>b:
+#     print("largest is a ")
+# else: 
+#     print("largest is b  ")
+
+# 3
+# a=int(input("enter a num"))
+# if a>0:
+#     print("number is positive")
+# else: 
+#     print("number is negative")
+
+# # 4
+# x=input("enter a letter")
+# if x=="a" or x=="e" or x=="i" or x=="o" or x=="u":
+#     print("letter is vowel")
+# else:
+#     print("letter is consonant")
+
+# # 5
+# score=int(input("enter your score :"))
+# if score>=90: 
+#     print("excellent score!!")
+# elif score>=80:
+#     print("very good score!!")
+# elif score>=70:
+#     print("good score!!")
+# else:
+#     print("poor, need to improve.")
+
+# #6
+# a=int(input("enter 1st num"))
+# b=int(input("enter 2nd num"))
+# c=int(input("enter 3RD num"))
+# # greatest
+# if a>b and a>c:
+#     print("a is greatest")
+# elif  b>c:
+#     print("b is greatest")
+# else:
+#     print("c is greatest")
+
+# # smallest
+# if a<b and a<c:
+#     print("a is smallest")
+# elif  b<c:
+#     print("b is smallest")
+# else:
+#     print("c is smallest")
+
+# # 7
+# a=int(input("enter a num"))
+# if a%2==0:
+#     print("number is even")
+# else:
+#     print("num is odd")
+
+# # 8
+# year=int(input("enter a year"))
+# if year%4==0:
+#         print("leap year")
+# else: 
+#         print("not a leap year")
